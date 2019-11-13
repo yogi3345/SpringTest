b82ed7be034b71278431005c17f5f42b6598e235
Yogesh <Yogesh@Predator>
2019-11-13 11:47:27 +0530

    Spring starter

---
+++ ekart/src/test/java/com/infy/ekart/EkartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infy.ekart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EkartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
