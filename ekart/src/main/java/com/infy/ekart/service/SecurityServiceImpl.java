10ad2a7dc9a76b215f9d16b159684b465d7103d2
Yogesh <Yogesh@Predator>
2019-11-22 13:54:46 +0530

    validation changes*

--- ekart/src/main/java/com/infy/ekart/service/SecurityServiceImpl.java
+++ ekart/src/main/java/com/infy/ekart/service/SecurityServiceImpl.java
@@ -1,7 +1,5 @@
 package com.infy.ekart.service;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -17,8 +15,6 @@ public class SecurityServiceImpl implements SecurityService{
 
     @Autowired
     private UserDetailsService userDetailsService;
-
-    private static final Logger logger = LoggerFactory.getLogger(SecurityServiceImpl.class);
 
     @Override
     public String findLoggedInUsername() {
@@ -36,7 +32,6 @@ public class SecurityServiceImpl implements SecurityService{
         authenticationManager.authenticate(usernamePasswordAuthenticationToken);
         if (usernamePasswordAuthenticationToken.isAuthenticated()) {
             SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
-            logger.debug(String.format("Auto login %s successfully!", username));
         }
     }
 }
