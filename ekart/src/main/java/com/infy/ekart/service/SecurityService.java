59401d709edb4311ad3c52d61bbe05ead0a1b54f
Yogesh <Yogesh@Predator>
2019-11-25 14:58:48 +0530

    Comments deleted

--- ekart/src/main/java/com/infy/ekart/service/SecurityService.java
+++ ekart/src/main/java/com/infy/ekart/service/SecurityService.java
@@ -1,7 +1,6 @@
 package com.infy.ekart.service;
 
 public interface SecurityService {
-    String findLoggedInUsername();
-
+	
     void autoLogin(String username, String password);
 }
--- ekart/src/main/java/com/infy/ekart/service/SecurityServiceImpl.java
+++ ekart/src/main/java/com/infy/ekart/service/SecurityServiceImpl.java
@@ -17,15 +17,6 @@ public class SecurityServiceImpl implements SecurityService{
     private UserDetailsService userDetailsService;
 
     @Override
-    public String findLoggedInUsername() {
-        /*Object userDetails = SecurityContextHolder.getContext().getAuthentication().getDetails();
-        if (userDetails instanceof UserDetails) {
-            return ((UserDetails)userDetails).getUsername();
-        }*/
-        return null;
-    }
-
-    @Override
     public void autoLogin(String username, String password) {
         UserDetails userDetails = userDetailsService.loadUserByUsername(username);
         UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(userDetails, password, userDetails.getAuthorities());
